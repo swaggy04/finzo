e5dab5e9e5a2c1cffb19a71e5fedc10bad876be8
swagatpadhi <litan3104@gmail.com>
2025-09-04 10:39:51 +0530

    new added

--- actions/addtransaction.js
+++ actions/addtransaction.js
@@ -1,7 +1,6 @@
 'use server';
 import { db } from "@/lib/db";
-
-import { auth } from "@clerk/nextjs/dist/types/server";
+import { auth } from "@clerk/nextjs/server";
 import { revalidatePath } from "next/cache";
 
 export async function addTransaction(data) {
--- app/(main)/transaction/_components/addtransaction.jsx
+++ app/(main)/transaction/_components/addtransaction.jsx
@@ -2,39 +2,69 @@
 
 import { addTransaction } from "@/actions/addtransaction"
 import { transactionSchema } from "@/app/lib/schema"
+import { Input } from "@/components/ui/input"
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
 import useFetch from "@/hooks/use-fetch"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 
-const AddTransactionForm = ({}) => {
-    useForm({
+const AddTransactionForm = ({ accounts, categories }) => {
+    const { register,
+        handleSubmit,
+        formState: { errors },
+        watch,
+        setValue,
+        reset,
+        getValues,
 
+    } = useForm({
         resolver: zodResolver(transactionSchema),
-        defaultValues:{
+        defaultValues: {
             type: "EXPENSE",
             amount: "",
-            description: "", 
-            accountid:accounts.find((ac)=> ac.isDefault)?.id || "",
+            description: "",
+            accountid: accounts.find((ac) => ac.isDefault)?.id || "",
             date: new Date(),
             isRecurring: false,
 
         }
     })
     const {
-        loading:transactionLoading,
-        fn:transactionFn,
-        data:transactionResult
+        loading: transactionLoading,
+        fn: transactionFn,
+        data: transactionResult
 
-    }=useFetch(addTransaction)
+    } = useFetch(addTransaction)
 
 
-  return (
-    <form action="">
-        <div>
-            <label htmlFor="">Type</label>
-        </div>
-    </form>
-  )
+    return (
+        <form className=" bg-amber-50 text-black  w-4xl flex flex-col p-6 rounded-lg shadow-md mx-auto my-10">
+            <div className="space-y-3">
+                <label className="text-sm font-bold  bg-amber-200  ">Type</label>
+                <Select onvalueChange={(value)=>setValue("type",value)} defaultValue={(watch("type"))} className="mt-3">
+                    <SelectTrigger className="w-full" >
+                        <SelectValue placeholder="Type" />
+                    </SelectTrigger>
+                    <SelectContent>
+                        <SelectItem value="INCOME">Income</SelectItem>
+                        <SelectItem value="EXPENSE">Expense</SelectItem>
+                    </SelectContent>
+                </Select>
+                {errors.type && <p className="text-sm text-red-500">{errors.type.message}</p>}
+            </div>
+            <div className="space-y-3">
+                <label className="text-sm font-bold  bg-amber-200  ">Type</label>
+                <Input
+                type="number"
+                step="0.01"
+                placeholder="Amount"
+                {...register("amount")}
+                />
+            
+                {errors.type && <p className="text-sm text-red-500">{errors.type.message}</p>}
+            </div>
+        </form>
+    )
 }
 
 export default AddTransactionForm
